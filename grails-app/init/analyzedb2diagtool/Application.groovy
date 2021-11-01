713252977009411dae4c8014ccba719666c55b19
jameschang <jameschangworking@gmail.com>
2021-11-01 19:10:57 +0800

    部屬Grails

---
+++ grails-app/init/analyzedb2diagtool/Application.groovy
@@ -0,0 +1,13 @@
+package analyzedb2diagtool
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
